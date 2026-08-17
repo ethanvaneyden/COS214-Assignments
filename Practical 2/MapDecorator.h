c9e44df507d0321c3ff2f6b93760a086e64db6d2
lesegot <lesegot2005@gmail.com>
2026-08-17 21:38:29 +0200

    feature/decorator: Added composite operations in MapDecorator.h

--- Practical 2/MapDecorator.h
+++ Practical 2/MapDecorator.h
@@ -5,16 +5,22 @@
 
 class MapDecorator : public Map{
     protected:
-        Map* wrapped;  // OWNED by this decorator!
+        Map* wrapped;  // OWNED by this decorator
 
     public:
         MapDecorator(Map* place);
 
+        // Delegating all operations to wrapped (default behaviour)
         std::string getName() const override;
         void describe() const override;
         int getPopulation() const override;
-        void growPopulation(int amount);
-        void decreasePopulation(int amount);
+        void growPopulation(int amount) override;
+        void decreasePopulation(int amount) override;
+
+        virtual void add(Map* child) override;
+        virtual void remove(Map* child) override;
+        virtual Map* getChild(int index) const override;
+        virtual int getChildCount() const override;
 
         ~MapDecorator() override;
 };

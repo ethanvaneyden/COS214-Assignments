e59a2684a6bc52b23b1bfc387bf81c61ce4c8d3c
lesegot <lesegot2005@gmail.com>
2026-08-17 20:45:56 +0200

    feature/decorator Added function definition in MapDecorator.h

--- Practical 2/MapDecorator.h
+++ Practical 2/MapDecorator.h
@@ -1,8 +1,22 @@
 #ifndef MAPDECORATOR_H
 #define MAPDECORATOR_H
 
-class MapDecorator{
+#include "Map.h"
 
+class MapDecorator : public Map{
+    protected:
+        Map* wrapped;  // OWNED by this decorator!
+
+    public:
+        MapDecorator(Map* place);
+
+        std::string getName() const override;
+        void describe() const override;
+        int getPopulation() const override;
+        void growPopulation(int amount);
+        void decreasePopulation(int amount);
+
+        ~MapDecorator() override;
 };
 
 #endif

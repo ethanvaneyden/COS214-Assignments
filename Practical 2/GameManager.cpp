6d7de292bc52ea098b3360ecfbf4e57d1cae354c
lesegot <lesegot2005@gmail.com>
2026-08-18 05:15:34 +0200

    Implemented GameManager.cpp

---
+++ Practical 2/GameManager.cpp
@@ -0,0 +1,90 @@
+#include "GameManager.h"
+#include "Traveller.h"
+#include "TravelState.h"
+#include "Map.h"
+#include "Location.h"
+#include "BiomeFactory.h"
+#include "Trip.h"
+
+GameManager::GameManager()
+    : traveller(nullptr), world(nullptr), trip(nullptr)
+{}
+
+GameManager::~GameManager(){
+    if(traveller != nullptr)
+        delete traveller;
+
+    if(world != nullptr)
+        delete world;
+
+    if(trip != nullptr)
+        delete trip;
+}
+
+void GameManager::setWorld(Map* world){
+    this->world = world;
+}
+
+Map* GameManager::getWorld() const{
+    return world;
+}
+
+void GameManager::setTraveller(Traveller* traveller){
+    this->traveller = traveller;
+}
+
+Traveller* GameManager::getTraveller() const{
+    return traveller;
+}
+
+void GameManager::setTrip(Trip* trip){
+    this->trip = trip;
+}
+
+Trip* GameManager::getTrip() const{
+    return trip;
+}
+
+void GameManager::setState(TravelState* state){
+    if(traveller != nullptr)
+        traveller->setState(state);
+}
+
+TravelState* GameManager::getState() const{
+    if(traveller == nullptr)
+        return nullptr;
+
+    return traveller->getCurrentState();
+}
+
+void GameManager::displayStateMenu(){
+    if(traveller == nullptr)
+        return;
+
+    TravelState* state = traveller->getCurrentState();
+
+    if(state != nullptr)
+        state->displayMenu();
+}
+
+void GameManager::handleInput(std::string input){
+    if(traveller == nullptr)
+        return;
+
+    TravelState* state = traveller->getCurrentState();
+
+    if(state != nullptr)
+        state->handleInput(input, traveller);
+}
+
+void GameManager::assignBiome(Map* location, BiomeFactory* factory){
+    if(location == nullptr || factory == nullptr)
+        return;
+
+    Location* currentLocation = dynamic_cast<Location*>(location);
+
+    if(currentLocation == nullptr)
+        return;
+
+    currentLocation->setBiome(factory->createBiome());
+}

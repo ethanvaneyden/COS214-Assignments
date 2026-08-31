3de9ae8c6ff45eca9f722d6584ff54f4ba9d4cac
MogaleLebethe <gales1.mogale@gmail.com>
2026-08-31 12:19:54 +0200

    Implement MapBuilder::build method to create exhibition components

--- Practical 3/MapBuilder.cpp
+++ Practical 3/MapBuilder.cpp
@@ -1,4 +1,10 @@
 #include "MapBuilder.h"
 #include "ExpoRegistry.h"
 
-void MapBuilder::build(ExpoRegistry &registry) {}
+void MapBuilder::build(ExpoRegistry& registry) {
+    registry.createComponent(ExpoRegistry::ComponentType::EXHIBITION_HALL);
+    registry.createComponent(ExpoRegistry::ComponentType::MAIN_STAGE, "Main Exhibition Hall");
+    registry.createComponent(ExpoRegistry::ComponentType::KEYNOTE_AREA, "Main Stage");
+    registry.createComponent(ExpoRegistry::ComponentType::DEMO_AREA, "Main Stage");
+
+}
